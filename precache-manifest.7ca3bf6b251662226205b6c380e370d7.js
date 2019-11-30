ff6f8479568f68a21f13b650ec1b4824df3d6f8f
Adrián Rossino <gurdoman@hotmail.com>
2019-11-29 23:38:56 -0600

    Updates

---
+++ precache-manifest.7ca3bf6b251662226205b6c380e370d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c106005fd3010a7745dbce20b683d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "736e87e8cc91747612d2",
+    "url": "/static/css/main.4df395ce.chunk.css"
+  },
+  {
+    "revision": "3daebaa2118b130afd44",
+    "url": "/static/js/2.313d0a86.chunk.js"
+  },
+  {
+    "revision": "736e87e8cc91747612d2",
+    "url": "/static/js/main.53fff782.chunk.js"
+  },
+  {
+    "revision": "45093b528c8dca09661b",
+    "url": "/static/js/runtime-main.592e2d25.js"
+  }
+]);
